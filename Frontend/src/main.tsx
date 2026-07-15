d4936603cc68ae23978a7b4fb4083e2b4b4bb3af
Collins526 <collins526@gmail.com>
2026-07-15 11:13:54 +0300

    everything

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
+import { setAuthTokenGetter, setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
 
+setBaseUrl(import.meta.env.VITE_API_BASE_URL ?? null);
 setAuthTokenGetter(() => localStorage.getItem("ryln_token"));
 
 createRoot(document.getElementById("root")!).render(<App />);
